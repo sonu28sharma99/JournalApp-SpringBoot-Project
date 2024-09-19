b84a255e96a9365d8d2432cb6a8c90344ecf40e6
Sonu Sharma <sonusharma@Sonus-Mac.local>
2024-09-20 04:41:50 +0530

    lombok is configured

--- src/main/java/com/sonusharma/journelApp/entity/JournalEntry.java
+++ src/main/java/com/sonusharma/journelApp/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.sonusharma.journelApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +11,7 @@ import java.time.LocalDate;
 import java.util.Date;
 
 @Document(collection = "journal_entries") // kind of row in db
+@Data
 public class JournalEntry {
     @Id // primary key
     private ObjectId id;
@@ -15,35 +19,4 @@ public class JournalEntry {
     private String content;
     private LocalDate date = LocalDate.now();
 
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
