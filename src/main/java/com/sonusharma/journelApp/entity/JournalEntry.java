04c61029a897ac684ca3d9cbc69280ae2d8e78ce
Sonu Sharma <sonusharma@Sonus-Mac.local>
2024-09-20 03:31:05 +0530

    date bug is fixed now

--- src/main/java/com/sonusharma/journelApp/entity/JournalEntry.java
+++ src/main/java/com/sonusharma/journelApp/entity/JournalEntry.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Document(collection = "journal_entries") // kind of row in db
@@ -12,14 +13,14 @@ public class JournalEntry {
     private ObjectId id;
     private String title;
     private String content;
-    private Date data;
+    private LocalDate date = LocalDate.now();
 
-    public Date getData() {
-        return data;
+    public LocalDate getDate() {
+        return date;
     }
 
-    public void setData(Date data) {
-        this.data = data;
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 
     public ObjectId getId() {
